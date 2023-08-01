mshr-h/lightning-thunder

commit c9d243bfc74fcc4bae9d9aefa704d7d949eefe75

Storing attributes (#499)
* store attr beginning

* snapshot of WIP for STORE_ATTR on modules

* misc

* update

* update

* lots of debugging output

* updating self.rope_cache works

* test fixes

* Pick from other branch.

* Taylor's better modify_copy_instruction. Thank you!

* fixes after merge

* fix attn

* drop unused class

* fix merge glitch

* refuse nested assignment

* add X_THUNDER_STORE_ATTR named constant

---------

Co-authored-by: Taylor Robie <taylor.aiken.robie@gmail.com>

thunder/__init__.py
@@ -201,7 +201,11 @@ def preprocess(fn, is_module):
     gr = script.frontend.acquire_method(fn.forward if is_module else fn)
     thunder.core.script.passes.unroll_for_loops_and_inline_modules(gr)
     if is_module:
-        additional_param_names, additional_param_values = thunder.core.script.passes.module_to_function(gr)
+        (
+            additional_param_names,
+            additional_param_values,
+            additional_return_names,
+        ) = thunder.core.script.passes.module_to_function(gr)
     script.passes.strongly_inline_functions(gr)
     script.passes.torch_to_thunder(gr)
 
@@ -209,26 +213,45 @@ def preprocess(fn, is_module):
     if is_module:
         thunder_fn._additional_param_names = additional_param_names
         thunder_fn._additional_param_values = additional_param_values
+        thunder_fn._additional_return_names = additional_return_names
     else:
         thunder_fn._additional_param_names = None
         thunder_fn._additional_param_values = None
+        thunder_fn._additional_return_names = None
     return thunder_fn
 
 
 class ThunderOptimizedModule(pytorch.nn.Module):  # TOM
     # todo: subclass nn.Module or forward things like .state_dict() to the
     #       model
-    def __init__(self, model, fn, tfn, additional_param_names, additional_param_values):
+    def __init__(self, model, fn, tfn, additional_param_names, additional_param_values, additional_return_names):
         super().__init__()
         self._model = model
         self._forward_fn = fn
         self._tfn = tfn
         self._additional_param_values = additional_param_values
         self._additional_param_names = additional_param_names
+        self._additional_return_names = additional_return_names
+        d = {k: i for i, k in enumerate(additional_param_names)}
+        self._additional_return_param_idxes = [d[k] for k in additional_return_names]
 
     def __call__(self, *args, **kwargs):
         all_args = (*self._additional_param_values, *args)
         res = self._forward_fn(*all_args, **kwargs)
+        if self._additional_return_names:
+            res, *additional_returns = res
+            assert len(self._additional_return_names) == len(
+                additional_returns
+            ), f"Number of expected additional return args {len(self._additional_return_names)=} does not match the actual number {len(additional_returns)=}"
+            for k, v, idx in zip(
+                self._additional_return_names, additional_returns, self._additional_return_param_idxes
+            ):
+                m = self._model
+                parts = k.split(".")
+                for p in parts[:-1]:
+                    m = getattr(m, p)
+                setattr(m, parts[-1], v)
+                self._additional_param_values[idx] = v
         return res
 
 
@@ -436,7 +459,9 @@ def compile(
             }
             _fn = thunder_backward(**compile_config)(pfn)
 
-        _fn = ThunderOptimizedModule(fn, _fn, pfn, pfn._additional_param_names, pfn._additional_param_values)
+        _fn = ThunderOptimizedModule(
+            fn, _fn, pfn, pfn._additional_param_names, pfn._additional_param_values, pfn._additional_return_names
+        )
         # TODO Revisit assuming these are const
         _fn._num_constant_args = len(pfn._additional_param_values)
 
@@ -478,6 +503,7 @@ def compile_with_info(fn, **compile_kwargs) -> Callable:
         result = cfn(*args, **kwargs)
         return result, last_traces(cfn)
 
+    _fn._fn = cfn
     return _fn
 
 

thunder/tests/test_script.py
@@ -355,7 +355,7 @@ def test_nanogpt_functionalization():
 
     gr = thunder.core.script.frontend.acquire_method(m.forward)
     thunder.core.script.passes.unroll_for_loops_and_inline_modules(gr)
-    additional_param_names, additional_param_values = thunder.core.script.passes.module_to_function(gr)
+    additional_param_names, additional_param_values, _ = thunder.core.script.passes.module_to_function(gr)
     thunder.core.script.graph.check_graph(gr)
 
     fn = thunder.core.script.python_ir.generate_function(gr)
@@ -782,8 +782,8 @@ def test_partial():
     assert expected == actual
 
 
+@skipif_not_python_3_10
 def test_foreign_store_attr():
-    @skipif_not_python_3_10
     def fn(o):
         o.hi = "hello"
         return 2
@@ -794,6 +794,50 @@ def test_foreign_store_attr():
     o = A()
     tfn = thunder.compile(fn)
     tfn(o)
+
+
+class TestStoreAttr1(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.first_x = None
+
+    def forward(self, x):
+        if self.first_x is None:
+            self.first_x = x
+        return 2 * x
+
+
+@skipif_not_python_3_10
+def test_store_attr_mod1():
+    m = TestStoreAttr1()
+
+    tom = thunder.compile(m)
+    x = torch.randn(5, 5)
+    m(x)
+    tom(x)
+    m(x * 2)
+    tom(x * 2)
+    assert_close(m.first_x, tom._model.first_x)
+
+
+class TestStoreAttr2(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.first_x = None
+
+    def forward(self, x):
+        if self.first_x is None:
+            # this is not allowed (we don't want to deal with the ordering issues)
+            self.first_x = x
+            self.first_x.xx = x
+        return 2 * x
+
+
+@skipif_not_python_3_10
+def test_store_attr_mod2():
+    m = TestStoreAttr2()
+    with pytest.raises(RuntimeError, match="members of.*members"):
+        tom = thunder.compile(m)
 
 
 @skipif_not_python_3_10

thunder/core/script/passes.py
@@ -23,6 +23,8 @@ from thunder.core.script.graph import (
 )
 from thunder.core.script.python_ir_data import (
     get_instruction,
+    modify_copy_instruction,
+    X_THUNDER_STORE_ATTR,
 )
 from thunder.torch import _torch_to_thunder_complete_map
 from thunder.core.utils import OrderedSet
@@ -665,6 +667,36 @@ def module_to_function(gr: "Graph") -> tuple[list[str], list[torch.Tensor]]:
     attr_dict: dict[str, int] = {}
     attr_list: list[str] = []
     attr_values = []
+    return_values: Dict[str, Value] = {}  # PhiValues in the return block
+
+    return_block = None
+    for bl in gr.blocks:
+        if bl.nodes[-1].i.opname == "RETURN_VALUE":
+            assert return_block is None, "multiple return statements should not happen here"
+            return_block = bl
+    assert return_block is not None, "could not find return block"
+
+    for bl in gr.blocks:
+        for n in bl.nodes:
+            if n.i.opname == "STORE_ATTR":
+                v = find_and_evaluate_method_through_phi_parent(n.inputs[1])
+                maybe_self, attrs = find_method_through_phi_parent(n.inputs[1])
+                attrs.append(n.i.argval)
+                if maybe_self.value is gr.module:
+                    attr_string = ".".join(attrs)
+                    n.i = modify_copy_instruction(n.i, opname=X_THUNDER_STORE_ATTR, opcode=None, argval=attr_string)
+                    pv = return_values.get(attr_string)
+                    if pv is None:
+                        pv = PhiValue([], [], return_block)
+                        pv.name = attr_string
+                        return_values[attr_string] = pv
+                        return_block.block_inputs.append(pv)
+                    v = Value(node=n, name=attr_string)  # disambiguate?
+                    pv.add_missing_value(v, jump_source=bl.nodes[-1])
+                    n.outputs = [v]
+                    bl.block_outputs.add(v)
+                    del n.inputs[1]
+
     for bl in gr.blocks:
         for n in bl.nodes:
             for idx_i, i in enumerate(n.inputs):
@@ -672,8 +704,8 @@ def module_to_function(gr: "Graph") -> tuple[list[str], list[torch.Tensor]]:
                 v = find_and_evaluate_method_through_phi_parent(i)
                 maybe_self, attrs = find_method_through_phi_parent(i)
 
-                if maybe_self.value is gr.module and isinstance(v, torch.Tensor):
-                    attr_string = ".".join(attrs)
+                attr_string = ".".join(attrs)
+                if maybe_self.value is gr.module and (isinstance(v, torch.Tensor) or (attr_string in return_values)):
                     # the new attributes come directly after the self argument
                     idx = attr_dict.setdefault(attr_string, len(attr_list) + 1)
                     if idx == len(attr_list) + 1:
@@ -684,6 +716,8 @@ def module_to_function(gr: "Graph") -> tuple[list[str], list[torch.Tensor]]:
                         gr.co_argcount += 1
                         # we need a default argument to be able to put the things at the end (but this will have to change for *args, **kwargs anyway...
                         # gr.func_defaults.append(None)
+                        if attr_string in return_values:
+                            return_values[attr_string].add_missing_value(func_arg)
                     else:
                         func_arg = gr.local_variables_at_start[idx]
 
@@ -706,6 +740,17 @@ def module_to_function(gr: "Graph") -> tuple[list[str], list[torch.Tensor]]:
                     i.is_const = True
                     i.is_global = False
 
+    if return_values:
+        bt_extra = Node(
+            i=get_instruction(opname="BUILD_TUPLE", arg=1 + len(return_values)), line_no=return_block.nodes[-1].line_no
+        )
+        bt_extra.inputs = return_block.nodes[-1].inputs + list(return_values.values())
+        v_tuple_extra = Value(node=bt_extra)
+        bt_extra.outputs = [v_tuple_extra]
+        return_block.nodes.insert(-1, bt_extra)
+        return_block.nodes[-1].inputs = [v_tuple_extra]
+    # gr.summary(print_lines=True)
+
     remove_unused_values(gr)
     if gr.local_variables_at_start[0].phi_values:
         raise RuntimeError(
@@ -713,6 +758,22 @@ def module_to_function(gr: "Graph") -> tuple[list[str], list[torch.Tensor]]:
     this most likely means that you are setting attributes in forward or using them
     in an unexpected way that thunder does not yet support."""
         )
+
+    # check to avoid assignments for both a.b and a.b.c
+    sorted_keys = sorted(return_values.keys())  # this uses that '.' sorts before other things
+    for i in range(len(sorted_keys) - 1):
+        kbase = sorted_keys[i]
+        knext = sorted_keys[i + 1]
+        if knext.startswith(kbase) and knext[len(kbase)] == ".":
+            # N.B. we know that knext is longer if kbase is a prefix so the knext[len(kbase)] above will not be out of bounds.
+            raise RuntimeError(f"Assigning to members of assigned members ('{kbase}' and '{knext}') is not supported.")
+
     del gr.local_variables_at_start[0]
     gr.co_argcount -= 1
-    return attr_list, attr_values
+    if gr.co_posonlyargcount > 0:
+        gr.co_posonlyargcount -= 1
+
+    # thunder.core.script.graph.check_graph(gr)
+    # gr.summary(print_lines=True)
+
+    return attr_list, attr_values, list(return_values.keys())

thunder/core/script/python_ir.py
@@ -14,7 +14,7 @@ from thunder.core.script.graph import (
     insert_before,
     insert_after,
 )
-from thunder.core.script.python_ir_data import get_instruction, modify_copy_instruction
+from thunder.core.script.python_ir_data import get_instruction, modify_copy_instruction, RETURN_VALUE, X_THUNDER_STORE_ATTR
 from thunder.core.utils import OrderedSet
 
 
@@ -216,8 +216,9 @@ def undo_ssa(gr: "Graph") -> tuple[list[Value], list[str], list[str], list[Any]]
                         idx = len(names)
                         names.append(n.i.argval)
                     n.i = modify_copy_instruction(n.i, arg=idx)
-
-        if bl.nodes[-1].i.opname != "RETURN_VALUE":  # TODO Should the return block have outputs (probably not)
+                if n.i.opname == X_THUNDER_STORE_ATTR:
+                    bl.nodes.remove(n)
+        if bl.nodes[-1].i.opname != RETURN_VALUE:  # TODO Should the return block have outputs (probably not)
             for o in bl.block_outputs:
                 if o not in processed_block_outputs:
                     get_value(o, n=jump_node)  # before the jump

thunder/core/script/python_ir_data.py
@@ -18,6 +18,7 @@ logger = logging.getLogger(__name__)
 SUPPORTS_PREPROCESSING = (3, 9) <= sys.version_info < (3, 11)
 JUMP_ABSOLUTE = "JUMP_ABSOLUTE"
 RETURN_VALUE = "RETURN_VALUE"
+X_THUNDER_STORE_ATTR = "X_THUNDER_STORE_ATTR"
 
 
 class VariableScope(enum.Enum):

thunder/tests/lit_llama_model.py
@@ -120,17 +120,17 @@ class CausalSelfAttention(nn.Module):
         q = q.view(B, T, self.n_head, head_size).transpose(1, 2)  # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, head_size).transpose(1, 2)  # (B, nh, T, hs)
 
-        # if self.rope_cache is None:
-        # cache for future forward calls
-        rope_cache = build_rope_cache(
-            seq_len=self.block_size,
-            n_elem=self.n_embd // self.n_head,
-            dtype=x.dtype,
-            device=x.device,
-        )
+        if self.rope_cache is None:
+            # cache for future forward calls
+            self.rope_cache = build_rope_cache(
+                seq_len=self.block_size,
+                n_elem=self.n_embd // self.n_head,
+                dtype=x.dtype,
+                device=x.device,
+            )
 
-        q = apply_rope(q, rope_cache)
-        k = apply_rope(k, rope_cache)
+        q = apply_rope(q, self.rope_cache)
+        k = apply_rope(k, self.rope_cache)
 
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         #  att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))

commit 68ebe0b8c102605e23faca3d78a5c96f1c8c31be

Hotfix for bug introduced by #499 in benchmark with backward (#740)
Thank you @kevinstephano for pointing this out.

thunder/__init__.py
@@ -760,7 +760,9 @@ def _make_trace(
         return trace
 
     if isinstance(fn, pytorch.nn.Module):
-        wrapped = ThunderOptimizedModule(fn, wrapped, pfn, pfn._additional_param_names, pfn._additional_param_values)
+        wrapped = ThunderOptimizedModule(
+            fn, wrapped, pfn, pfn._additional_param_names, pfn._additional_param_values, pfn._additional_return_names
+        )
 
     return wrapped
 

thunder/tests/test_script.py
@@ -228,9 +228,18 @@ def test_llama_block_compile():
     thunder_result = tom(inp)
     assert_close(expected_result, thunder_result)
     assert_close(m.attn.rope_cache, m2.attn.rope_cache)
+
+    # TODO: enable:
+    # inp = torch.randn(1, 8, 4096, requires_grad=True)
     expected_result = m(inp)
     thunder_result = tom(inp)
     assert_close(expected_result, thunder_result)
+
+    # go = torch.ones(expected_result.shape, dtype=expected_result.dtype, device=expected_result.device)
+
+    # expected_gr, = torch.autograd.grad(expected_result, inp, go)
+    # thunder_gr, = torch.autograd.grad(thunder_result, inp, go)
+    # assert_close(expected_result, thunder_result)
 
 
 @skipif_not_python_3_10